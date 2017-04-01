4b6617bb1bb233bfd5ef1ff0d4bc647f6e6dd765
Rangga Adhitya <deneuv3.4@gmail.com>
2017-04-01 13:47:59 +0700

    make websocket server

--- websocket/websocket.js
+++ websocket/websocket.js
@@ -1,11 +1,23 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-
-io.on('connection', function(client){
-  client.on('event', function(data){});
-  client.on('disconnect', function(){});
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+ 
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
 });
-
-server.listen(3000, () => {
-  console.log('SERVER RUNNING')
+ 
+app.get('/', function(req, res, next){
+  console.log('user connected', req.testing);
+  res.end();
 });
+ 
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+ 
+app.listen(3000);
---
+++ websocket/websocketTryout/index.android.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  WebSocket,
+} from 'react-native';
+
+export default class websocketTryout extends Component {
+  componentDidMount() {
+  }
+
+  webSock() {
+    var ws = new WebSocket('ws://192.168.56.1:3000/');
+
+    ws.onopen = () => {
+      // connection opened
+
+      ws.send('something'); // send a message
+    };
+
+    ws.onmessage = (e) => {
+      // a message was received
+      console.log(e.data);
+    };
+
+    ws.onerror = (e) => {
+      // an error occurred
+      console.log(e.message);
+    };
+
+    ws.onclose = (e) => {
+      // connection closed
+      console.log(e.code, e.reason);
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('websocketTryout', () => websocketTryout);
